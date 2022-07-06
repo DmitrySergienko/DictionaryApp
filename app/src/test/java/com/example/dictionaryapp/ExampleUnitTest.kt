ae6a4d338f6a806c55f8c1a2d135ae81f64acc45
DmitrySergienko <dmitrisergienko@gmail.com>
2022-07-06 11:32:01 +0400

    Test example (interface testing)

--- app/src/test/java/com/example/dictionaryapp/ExampleUnitTest.kt
+++ app/src/test/java/com/example/dictionaryapp/ExampleUnitTest.kt
@@ -12,5 +12,46 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+
+
     }
+
+
+    @Test
+    fun testWithFake(){
+        val dependency = FakeTestInterface()
+        val targetClass = TargetClass(dependency)
+        targetClass.doAction()
+
+        assertEquals("test handled",dependency.lastTest2)
+
+    }
+}
+interface TestInterface{
+    fun test(): String
+    fun test2(str:String)
+}
+
+
+class TargetClass(private val dependency:TestInterface){
+    fun doAction(){
+        val test = dependency.test()
+        val handled ="$test handled"
+        dependency.test2(handled)
+    }
+}
+//Fake Test
+
+class FakeTestInterface:TestInterface{
+
+    var lastTest2:String? = null
+
+    override fun test(): String {
+        return "test"
+    }
+
+    override fun test2(str: String) {
+        lastTest2 = str
+    }
+
 }
